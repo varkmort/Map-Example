dd57d66e448467e09aae42c2d2094e62ab69a007
Vladislav Tsibart <vitsibart@gmail.com>
2022-07-20 19:14:20 +0500

    mid exampl

--- c++20 with bin output1/example.cpp
+++ c++20 with bin output1/example.cpp
@@ -1,10 +1,40 @@
 ﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
 #include "Init.hpp"
 
 int main()
 {
-	for (auto& record : GetNames()) {
-		std::cout << record << '\n';
+	std::vector<NameRecord> db = GetNames();
+	std::vector<std::string> names;
+	std::ifstream in("names.txt");
+	while (!in.eof()) {
+		std::string tmp;
+		in >> tmp;
+		names.push_back(tmp);
 	}
+	//
+	for (auto &name:names) {
+		auto res = std::find_if(
+			db.begin(), 
+			db.end(),
+			[&name](NameRecord &obj) {
+				if (obj.name == name) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		);
+		if (res == db.end()) {
+			std::cout << name << " wasnt found\n";
+		}
+		else {
+			std::cout << res->name << " has " << res->count << " owners\n";
+		}
+	}	
+	return 0;
 }
