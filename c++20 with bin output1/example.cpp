3dc44e257fcf4917b9a0727f4e4c99c6884bf8b2
Derevolaz1k <0_the_kick_0@mail.ru>
2022-07-20 21:11:02 +0500

    pull request

--- c++20 with bin output1/example.cpp
+++ c++20 with bin output1/example.cpp
@@ -31,8 +31,43 @@ void FindByVector(std::vector<NameRecord>& db, std::vector<std::string> &names)
 }
 
 
+
+void PrintContaint(std::map<std::string, NameRecord*> &searcTable, std::vector<std::string> &names)
+{
+	for (const auto name : names)
+	{
+		if (searcTable.contains(name))
+		{
+			std::cout << "Name: " << searcTable.at(name)->name << " id: " << searcTable.at(name)->id << std::endl;
+		}
+		else
+		{
+			std::cout << name << " wasnt found\n";
+		}
+	}
+		
+}
+
+std::vector<std::string> FindName(std::map<std::string, NameRecord*> searcTable, std::vector<std::string> names)
+{
+	std::vector<std::string> res;
+	for (auto& name : names) {
+		if (!searcTable.contains(name)) {
+			//std::cout << name << " wasnt found\n";
+		}
+		else if (searcTable.at(name)->count == 1)
+		{
+			/*std::cout << searcTable.at(name)->name << " has " <<
+				searcTable.at(name)->count << " owners\n";*/
+			res.push_back(name);
+		}
+	}
+	return res;
+}
+
 int main()
 {
+	system("chcp 65001");
 	std::vector<NameRecord> db = GetNames();
 	std::vector<std::string> names;
 	std::ifstream in("names.txt");
@@ -46,14 +81,12 @@ int main()
 	for (auto &record:db) {
 		searcTable.insert({record.name, &record});
 	}
-	for (auto &name : names) {
-		if (!searcTable.contains(name)) {
-			std::cout << name << " wasnt found\n";
-		}
-		else {
-			std::cout << searcTable[name]->name << " has " << 
-				searcTable[name]->count << " owners\n";
-		}
+	std::vector<std::string> namesCount1 = FindName(searcTable, names);
+	for (const auto it : namesCount1)
+	{
+		std::cout << it << '\n';
 	}
+	std::cout << std::endl;
+	PrintContaint( searcTable, names);
 	return 0;
 }
