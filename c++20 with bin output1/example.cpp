cb3b41e86967d1a7727c7c3af4755ec23bfb9791
Vladislav Tsibart <vitsibart@gmail.com>
2022-07-20 19:48:05 +0500

    1 less

--- c++20 with bin output1/example.cpp
+++ c++20 with bin output1/example.cpp
@@ -2,23 +2,15 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <map>
 
 #include "Init.hpp"
 
-int main()
+void FindByVector(std::vector<NameRecord>& db, std::vector<std::string> &names)
 {
-	std::vector<NameRecord> db = GetNames();
-	std::vector<std::string> names;
-	std::ifstream in("names.txt");
-	while (!in.eof()) {
-		std::string tmp;
-		in >> tmp;
-		names.push_back(tmp);
-	}
-	//
-	for (auto &name:names) {
+	for (auto &name : names) {
 		auto res = std::find_if(
-			db.begin(), 
+			db.begin(),
 			db.end(),
 			[&name](NameRecord &obj) {
 				if (obj.name == name) {
@@ -35,6 +27,33 @@ int main()
 		else {
 			std::cout << res->name << " has " << res->count << " owners\n";
 		}
-	}	
+	}
+}
+
+
+int main()
+{
+	std::vector<NameRecord> db = GetNames();
+	std::vector<std::string> names;
+	std::ifstream in("names.txt");
+	while (!in.eof()) {
+		std::string tmp;
+		in >> tmp;
+		names.push_back(tmp);
+	}
+	//
+	std::map<std::string, NameRecord *> searcTable;
+	for (auto &record:db) {
+		searcTable.insert({record.name, &record});
+	}
+	for (auto &name : names) {
+		if (!searcTable.contains(name)) {
+			std::cout << name << " wasnt found\n";
+		}
+		else {
+			std::cout << searcTable[name]->name << " has " << 
+				searcTable[name]->count << " owners\n";
+		}
+	}
 	return 0;
 }
